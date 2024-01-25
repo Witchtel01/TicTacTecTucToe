9176e769150f5509f042a5de292d6324441f0cc6
Witchtel <engineeringkerbals@gmail.com>
2024-01-25 15:19:59 -0500

    eugh

--- epilepsy warning.py
+++ epilepsy warning.py
@@ -1,14 +1,19 @@
 import pygame as pg
 
+print("Epilepsy Warning! Continue? (Y/n)")
+response = input()
+if response == "n" or response == "N":
+    exit()
 pg.init()
 screen = pg.display.set_mode((300,300), pg.RESIZABLE)
 clock = pg.time.Clock()
+pg.display.set_caption("Epilepsy Warning")
 
 running = True
 
 while running:
     for event in pg.event.get():
-        if event.type == pg.quit:
+        if event.type == pg.QUIT:
             running = False
         elif event.type == pg.VIDEORESIZE:
             pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
